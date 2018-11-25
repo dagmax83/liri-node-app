5c59ced5257cca659d71a03b13dfc21767bbdba4
DAGMAX83 <DAGMAX983@gmail.com>
2018-11-25 15:50:10 -0600

    created folders and added api keys

---
+++ keys.js
@@ -0,0 +1,6 @@
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.4b64e9ac130048c18dd2fd95d9edba9f,
+  secret: process.env.94bf34fcd5184d2e8c9517150aeb065e
+};
---
+++ liri.js
@@ -0,0 +1,35 @@
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: <4b64e9ac130048c18dd2fd95d9edba9f>,
+  secret: <4bf34fcd5184d2e8c9517150aeb065e>
+});
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
+
+axios.get('/user?ID=f680275c')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });const db = require('db')
+db.connect({
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS
+});
+
+const result = dotenv.config()
+ 
+if (result.error) {
+  throw result.error
+}
+ 
+console.log(result.parsed)
